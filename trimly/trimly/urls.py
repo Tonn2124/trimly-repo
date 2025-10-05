c30b1c1484046ba71a60e4b9b9f1816d2e5609b4
Lord Anthone Acebedo <anthoneacebedo24@gmail.com>
2025-10-05 13:51:25 +0800

    Changes Temporary: landing page at login for demo

--- trimly/trimly/urls.py
+++ trimly/trimly/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from main import views
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.login_view, name='login'),
+    path('', lambda request: redirect('login')),
     path('register/', views.registration_view, name='register'),
     path('dashboard/barber/', views.barber_dashboard, name='barber_dashboard'),
     path('dashboard/customer/', views.customer_dashboard, name='customer_dashboard'),
